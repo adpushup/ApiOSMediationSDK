e46772a3519f6ac483e99a7ef24fb394b9b2be70
rishh18 <rishabh.tripathi@adpushup.com>
2026-01-14 00:16:39 +0530

    Version 1.0.3

--- Package.swift
+++ Package.swift
@@ -13,11 +13,11 @@ let package = Package(
             targets: ["ApiOSMediationSDK"]
         ),
     ],
-    dependencies: [.package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.0.0")],
+    dependencies: [.package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.13.0")],
     targets: [
         .binaryTarget(
             name: "ApiOSMediationSDK",
-            url: "https://github.com/adpushup/ApiOSMediationSDK/releases/download/1.0.2/ApMediationiOSSDK.xcframework.zip",
+            url: "https://github.com/adpushup/ApiOSMediationSDK/releases/download/1.0.3/ApMediationiOSSDK.xcframework.zip",
             checksum: "5cac58c835c20638358816f375073886844110bcee819dab3318fcafccca0f1c"
         )
     ]
